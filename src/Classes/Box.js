8990726dec816c375b98e8a44f5e21f17a810b5b
Masood <masoodsse@gmail.com>
2023-12-26 19:00:53 +0500

    all boxes moved logic

--- src/Classes/Box.js
+++ src/Classes/Box.js
@@ -1,16 +1,30 @@
+import { useGameStore } from "@/stores/gamestore.js";
 export class Box {
   row = 0;
   col = 0;
+  lastCol = 0;
+  lastrow = 0;
   color = "green";
+
   constructor(row, col) {
+    const game = useGameStore();
     this.row = row;
     this.col = col;
   }
+  updatePosition() {
+    this.lastCol = this.col;
+    this.lastrow = this.row;
+  }
+  follow(box) {
+    this.updatePosition();
+    this.col = box.lastCol;
+    this.row = box.lastrow;
+  }
   move(direction) {
+    this.updatePosition();
     switch (direction) {
       case "U":
         this.row--;
-        this.col;
         break;
       case "D":
         this.row++;
--- src/Classes/Snake.js
+++ src/Classes/Snake.js
@@ -2,25 +2,30 @@ import { useGameStore } from "@/stores/gamestore.js";
 import { Box } from "./Box.js";
 export class Snake {
   boxes = [];
+  direction = "R";
   constructor() {
     const game = useGameStore();
     const center = Math.floor(game.totalBoxes / 2);
-    this.boxes.push(new Box(center, center - 2));
-    this.boxes.push(new Box(center, center - 1));
     this.boxes.push(new Box(center, center));
+    this.boxes.push(new Box(center, center - 1));
+    this.boxes.push(new Box(center, center - 2));
   }
-  move(direction) {
+  move() {
     const game = useGameStore();
-    let lastBox = this.boxes.at(-1);
-    let firstBox = this.boxes.at(0);
-    if (lastBox.col === game.totalBoxes - 1) return;
-    if (firstBox.col === 0) return;
-    if (lastBox.row === game.totalBoxes - 1) return;
-    if (firstBox.row === 0) return;
 
-    for (let box of this.boxes) {
-      box.move(direction);
+    let firstBox = this.boxes[0];
+    if (
+      firstBox.col === game.totalBoxes - 1 ||
+      firstBox.col === 0 ||
+      firstBox.row === game.totalBoxes - 1 ||
+      firstBox.row === 0
+    )
+      return;
+    firstBox.move(this.direction);
+    for (let i = 1; i < this.boxes.length; i++) {
+      let box = this.boxes[i];
+      let previous = this.boxes[i - 1];
+      box.follow(previous);
     }
-    console.log("first", this.firstBox);
   }
 }
