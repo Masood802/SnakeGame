e828139c6031588d92e053d64f8d77eea2b3d9e6
Masood <masoodsse@gmail.com>
2023-12-26 21:27:55 +0500

    CreateFoodItem Function created

--- src/stores/gamestore.js
+++ src/stores/gamestore.js
@@ -1,16 +1,30 @@
 import { defineStore } from "pinia";
+import { FoodItem } from "@/Classes/FoodItem";
 
 export const useGameStore = defineStore("game", {
   state: () => ({
     BoxSize: 30,
-    totalBoxes: 20,
-    speed: 500, // in ms
+    totalBoxes: 25,
+    speed: 300, // in ms
     snake: null,
-    direction:''
+    direction: "",
+    gameover: false,
+    paused: false,
+    Fooditem: null,
   }),
   getters: {
     gridSize() {
       return this.BoxSize * this.totalBoxes;
+    },
+  },
+  actions: {
+    Restart() {
+      window.location.reload();
+    },
+    CreateFoodItem() {
+      let row = Math.floor(Math.random() * this.totalBoxes);
+      let col = Math.floor(Math.random() * this.totalBoxes);
+      this.Fooditem = new FoodItem(row, col);
     },
   },
 });
