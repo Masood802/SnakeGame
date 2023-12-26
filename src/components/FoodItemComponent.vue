89732a8034d40f8eb19d5d668ab50744cbcc8f91
Masood <masoodsse@gmail.com>
2023-12-26 21:25:40 +0500

    foodItem Component created

---
+++ src/components/FoodItemComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <img src="/pix/blueLink.png" alt="" class="absolute" :style="Newstyles" />
+</template>
+
+<script setup>
+import { useGameStore } from "@/stores/gamestore";
+import { computed } from "vue";
+
+let game = useGameStore();
+const Newstyles = computed(() => {
+  return {
+    left: game.Fooditem.col * game.BoxSize + "px",
+    top: game.Fooditem.row * game.BoxSize + "px",
+    width: game.BoxSize + "px",
+    height: game.BoxSize + "px",
+  };
+});
+</script>
