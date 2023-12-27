4406a2298ee1af6491af7ed0856a1ef033d40f72
Masood <masoodsse@gmail.com>
2023-12-27 12:43:03 +0500

    paly button addded

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <main class="flex justify-center items-center h-[100vh]">
-    <RouterLink to="/game-view">Paly Game</RouterLink>
+  <main
+    class="bg-[url(./pix/peakpx.jpg)] flex-col bg-cover all-center flex h-[100vh] w-[100vw]"
+  >
+    <img
+      src="../../public/pix/title.png"
+      alt=""
+      class="absolute top-24 w-[40vw]"
+    />
+    <RouterLink
+      to="/game-view"
+      class="bg-[url(./pix/play.png)] bg-cover bg-center shadow rounded-full text-3xl text-green-600 w-24 h-24 font-bold"
+    ></RouterLink>
   </main>
 </template>
