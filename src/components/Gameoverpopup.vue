db96bd493967e5107d5be185d8bb77a35dfe30f2
Masood <masoodsse@gmail.com>
2023-12-28 22:05:29 +0500

    display score on gameover popup

--- src/components/Gameoverpopup.vue
+++ src/components/Gameoverpopup.vue
@@ -1,6 +1,7 @@
 <template>
   <ThePopup>
     <h1 class="text-4xl mb-5">Game Over</h1>
+    <h2>Your Score: {{ game.score }}</h2>
     <button
       class="text-lg font-bold bg-green-800 rounded-lg px-8 py-2 my-4 text-slate-50"
       @click.stop="game.Restart"
