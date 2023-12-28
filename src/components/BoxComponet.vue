6d9bfcc07bfc01e578e8e4d829dc756e96cb194e
Masood <masoodsse@gmail.com>
2023-12-28 16:03:47 +0500

    snake head picture updated

--- src/components/BoxComponet.vue
+++ src/components/BoxComponet.vue
@@ -1,6 +1,6 @@
 <template>
   <img
-    src="/pix/greenLink.png"
+    :src="src"
     alt=""
     class="absolute transition-all ease-linear"
     :style="styles"
@@ -9,19 +9,26 @@
 
 <script setup>
 import { useGameStore } from "@/stores/gamestore";
-import { computed } from "vue";
-
+import { computed} from "vue";
 let game = useGameStore();
+
+const src = computed(() => {
+  return p.box.isHead ? "/pix/SnakeHead.png" : `/pix/${p.box.color}Link.png`;
+});
+
 const p = defineProps({
   box: Object,
 });
 const styles = computed(() => {
+  const rotations = { U: 0, L: -90, D: 180, R: 90 };
+  const rotation = rotations[game.snake.direction];
   return {
     left: p.box.col * game.BoxSize + "px",
     top: p.box.row * game.BoxSize + "px",
     width: game.BoxSize + "px",
     height: game.BoxSize + "px",
     transitionDuration: game.speed + "ms",
+    transform: `rotate(${rotation}deg)`,
   };
 });
 </script>
