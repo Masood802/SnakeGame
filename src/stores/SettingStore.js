4b910b2221f5861c1dcc4d0afe150ed47395a0e7
Malik <vofene3689@wlmycn.com>
2024-01-11 21:04:18 +0500

    adjusted start game sounds

--- src/stores/SettingStore.js
+++ src/stores/SettingStore.js
@@ -1,10 +1,11 @@
 import { defineStore } from "pinia";
 
 export const useSettingStore = defineStore("setting", {
-    state: ()=>({
-        togglesetting: true,
-        gameSound: .3,
-        gameEffects: .5,
-        zenmode:false
-    })
-})
+  state: () => ({
+    togglesetting: true,
+    gameSound: 0.1,
+    gameEffects: 0.8,
+    zenmode: false,
+    gameDifficulty:null
+  }),
+});
